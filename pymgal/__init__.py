e43a1a5b1dfc0064db3d6c6d924a9e8b209c06bc
Weiguang Cui <weiguang.cui@uwa.edu.au>
2017-06-09 17:49:09 +0200

    add projection class to pymgal

--- pymgal/__init__.py
+++ pymgal/__init__.py
@@ -7,17 +7,14 @@ More details of the functions and models.
 
 __author__ = 'Weiguang Cui'
 __email__ = 'cuiweiguang@gmail.com'
-__ver__ = 'beta'
+__version__ = 'beta'
 
 # import numpy as np  # For modern purposes
 from pymgal.SSP_models import SSP_models
 from pymgal.load_data import load_data
 from pymgal.filters import filters
 from pymgal import utils
-# import astro_filter
-# import wrapper
-# import sfhs
-# import weight
+from pymgal.projection import projection
 from pymgal import dusts
 
 # ezsps = ezsps.ezsps
